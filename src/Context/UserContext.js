814c3fea48f2fa1a9075ff2a4f85091d304695dc
Masagus Hariadi Arief <adi.arief77@gmail.com>
2022-07-17 21:32:00 +0700

    apply prettier

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,92 +1,94 @@
 import { createContext, useState, useEffect } from 'react'
 import queryString from 'query-string'
-import API from "Services/API";
-import useDebounce from "Hook/useDebounce";
+import API from 'Services/API'
+import useDebounce from 'Hook/useDebounce'
 
 export const UserContext = createContext()
 export const UserConsumer = UserContext.Consumer
 
 const UserContextProvider = ({ children }) => {
-    const [users, setUsers] = useState({
-        isLoading: false,
-        items: [],
-    })
+   const [users, setUsers] = useState({
+      isLoading: false,
+      items: [],
+   })
 
-    const [gender, setGender] = useState('')
-    const [keyword, setKeyword] = useState('')
-    const debouncedSearch = useDebounce(keyword, 500);
+   const [gender, setGender] = useState('')
+   const [keyword, setKeyword] = useState('')
+   const debouncedSearch = useDebounce(keyword, 500)
 
-    const [pagination, setPagination] = useState({
-        page: 1,
-        pageSize: 5,
-        results: 5,
-    })
+   const [pagination, setPagination] = useState({
+      page: 1,
+      pageSize: 5,
+      results: 5,
+   })
 
-    const fetchUserData = () => {
-        let params = Object.fromEntries(Object.entries({
+   const fetchUserData = () => {
+      let params = Object.fromEntries(
+         Object.entries({
             gender,
             keyword,
-            ...pagination
-        }).filter(([_, v]) => v));
+            ...pagination,
+         }).filter(([_, v]) => v)
+      )
 
-        setUsers((prevState) => ({ ...prevState, isLoading: true }))
-        API.get(`/?${queryString.stringify(params)}`)
-            .then((response) => {
-                if (response.status === 200) {
-                    setUsers((prevState) => ({
-                        ...prevState,
-                        items: response.data.results.map((user, key) => ({
-                            key: key + 1,
-                            username: user.login.username,
-                            name: `${user.name.first} ${user.name.last}`,
-                            email: user.email,
-                            gender: user.gender,
-                            registeredData: user.registered.date,
-                        })),
-                    }))
-                }
-            })
-            .catch(() => console.log('Network error, try letter'))
-            .finally(() => setUsers((prevState) => ({ ...prevState, isLoading: false })))
-    }
+      setUsers((prevState) => ({ ...prevState, isLoading: true }))
+      API.get(`/?${queryString.stringify(params)}`)
+         .then((response) => {
+            if (response.status === 200) {
+               setUsers((prevState) => ({
+                  ...prevState,
+                  items: response.data.results.map((user, key) => ({
+                     key: key + 1,
+                     username: user.login.username,
+                     name: `${user.name.first} ${user.name.last}`,
+                     email: user.email,
+                     gender: user.gender,
+                     registeredData: user.registered.date,
+                  })),
+               }))
+            }
+         })
+         .catch(() => console.log('Network error, try letter'))
+         .finally(() => setUsers((prevState) => ({ ...prevState, isLoading: false })))
+   }
 
-    useEffect(fetchUserData, [gender, pagination, debouncedSearch])
+   useEffect(fetchUserData, [gender, pagination, debouncedSearch])
 
-    const paginationChange = (page, pageSize) => {
-        setPagination((prevState) => ({
-            ...prevState,
-            page,
-            pageSize,
-            results: pageSize
-        }))
-    }
+   const paginationChange = (page, pageSize) => {
+      setPagination((prevState) => ({
+         ...prevState,
+         page,
+         pageSize,
+         results: pageSize,
+      }))
+   }
 
-    const resetFilter = () => {
-        setGender('')
-        setKeyword('')
-        setPagination({
-            page: 1,
-            pageSize: 5,
-            results: 5,
-        })
-    }
+   const resetFilter = () => {
+      setGender('')
+      setKeyword('')
+      setPagination({
+         page: 1,
+         pageSize: 5,
+         results: 5,
+      })
+   }
 
-    return (
-        <UserContext.Provider
-            value={{
-                users,
-                gender,
-                setGender,
-                keyword,
-                setKeyword,
-                resetFilter,
-                pagination,
-                paginationChange
-            }}>
-            {children}
-        </UserContext.Provider>
-    )
-
+   return (
+      <UserContext.Provider
+         value={{
+            users,
+            gender,
+            setGender,
+            keyword,
+            setKeyword,
+            resetFilter,
+            pagination,
+            paginationChange,
+         }}
+      >
+         {children}
+      </UserContext.Provider>
+   )
 }
 
 export default UserContextProvider
